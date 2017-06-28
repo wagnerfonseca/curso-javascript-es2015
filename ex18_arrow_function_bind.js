4407169f9dacfb125e448ce3b87e869dddf529bc
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-06-27 23:21:29 -0300

    o escopo do this em um arrow function

--- ex18_arrow_function_bind.js
+++ ex18_arrow_function_bind.js
@@ -3,15 +3,23 @@ this.desc = 'sample with bind! context is module.exports';
 const obj = { desc: 'Object, I am' };
 
 const func = function() {
-	console.log(this);
+	console.log('O contexto é module.exports?', this === module.exports);
+	console.log('O contexto é global?', this === global);
+	console.log('O contexto é undefined?', this === undefined);
+	console.log('O contexto é null?', this === null);
+	console.log('O contexto é obj?', this === obj);
 }
 func();
 
+console.log('Realizando o bind()')
 const funcBind = func.bind(obj);
 funcBind();
 
-const arrowFunc = () => console.log(this);
+// com arrow function o module.exports continua sendo o this
+const arrowFunc = () => console.log('Arrow function o contexto do this é module.exports?', this === module.exports);
 arrowFunc();
 
+// mesmo realizando o bind o contexto do this continua sendo module.exports
+console.log('Realizando bind -  Arrow function')
 const arrowFuncBind = arrowFunc.bind(obj);
 arrowFuncBind();
