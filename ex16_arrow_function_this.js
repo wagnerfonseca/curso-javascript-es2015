83d65c017bfa4b92e5967e9d65b8c4ed706880c1
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-06-27 22:37:43 -0300

    o escopo do this em um arrow function

--- ex16_arrow_function_this.js
+++ ex16_arrow_function_this.js
@@ -2,10 +2,17 @@ this.desc = 'funny arrow function';
 
 console.log(this);
 
+// em uma funcao normal o this aponta para a variavel 'global'
 const func = function() {
 	console.log(this === global);
 }
 func();
 
+// em um arrow function aponta para 'module.exports'
 const arrowFunc = () => console.log(this, this === module.exports);
 arrowFunc();
+
+// fora do escopo de uma funcao o this aponta para 'module.exports' idêntico ao arrow function
+console.log(this === global)
+console.log(this === undefined)
+console.log(this === module.exports)
